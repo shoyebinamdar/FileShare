0a4c2dd4acb75ee2bc958a1e291ee27a954122e6
shoyeb.inamdar <shoyeb.ina.1990@gmail.com>
2019-08-11 20:19:47 +0530

    Added logger

--- src/main/java/client/FileClient.java
+++ src/main/java/client/FileClient.java
@@ -6,13 +6,18 @@ import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
+import org.apache.log4j.Logger;
+
+import server.FileServer;
+
 import config.ClientConfigurationReader;
 import config.Configuration;
 import config.ConfigurationReader;
 
 public class FileClient {
-	
-	private Socket s;
+	static final Logger logger = Logger.getLogger(FileServer.class);
+
+	private Socket socket;
 	private Configuration configuration;
 	
 	public FileClient(Configuration configuration) {
@@ -23,24 +28,32 @@ public class FileClient {
 	
 	private void connect() {
 		try {
-			s = new Socket(configuration.getHost(), configuration.getPort());
+			logger.info("Creating client socket on " + configuration.getHost() + ":" + configuration.getPort());
+			socket = new Socket(configuration.getHost(), configuration.getPort());
+			logger.info("Successfully created client socket");
 		} catch (UnknownHostException e) {
+			logger.error("Failed to create client socket. " + configuration.getHost() + " not found.");
 			e.printStackTrace();
 		} catch (IOException e) {
+			logger.error("Failed to create client socket.");
 			e.printStackTrace();
 		}
 	}
 	
 	private void sendFile() {
 		try {
-			DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+			DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
 			FileInputStream fis = new FileInputStream(configuration.getFile());
 			byte[] buffer = new byte[configuration.getPacketSize()];
+			int read = 0;
+			int totalWrite = 0;
 			
-			while (fis.read(buffer) > 0) {
+			logger.info("Now sending the contents of " + configuration.getFile() + " to server");
+			while ((read = fis.read(buffer)) > 0) {
+				totalWrite += read;
 				dos.write(buffer);
 			}
-			
+			logger.info("Successfully sent " + totalWrite + " bytes of data to server");
 			fis.close();
 			dos.close();
 		} catch (IOException e) {
--- src/main/java/server/FileServer.java
+++ src/main/java/server/FileServer.java
@@ -5,14 +5,17 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import org.apache.log4j.Logger;
 
 import config.Configuration;
 import config.ConfigurationReader;
 import config.ServerConfigurationReader;
 
 public class FileServer extends Thread {
+	static final Logger logger = Logger.getLogger(FileServer.class);
+
 	
-	private ServerSocket ss;
+	private ServerSocket serverSocket;
 	private Configuration configuration;
 	
 	public FileServer(Configuration configuration) {
@@ -22,16 +25,22 @@ public class FileServer extends Thread {
 	
 	private void connect() {
 		try {
-			ss = new ServerSocket(configuration.getPort());
+			logger.info("Creating server socket");
+			serverSocket = new ServerSocket(configuration.getPort());
+			logger.info("Server socket created successfully");
 		} catch (IOException e) {
 			e.printStackTrace();
+			logger.error("Failed to create server socket");
 		}
 	}
 	
 	public void run() {
 		while (true) {
 			try {
-				Socket clientSock = ss.accept();
+				logger.info("Server socket trying to accept connections on port:" + configuration.getPort());
+				Socket clientSock = serverSocket.accept();
+				logger.info("Server socket ready to accept data from port:" + configuration.getPort());
+				logger.info("Waiting for data");
 				saveFile(clientSock);
 			} catch (IOException e) {
 				e.printStackTrace();
@@ -47,11 +56,15 @@ public class FileServer extends Thread {
 		int read = 0;
 		int totalRead = 0;
 
+		if (dis.available() > 0)
+			logger.info("Data available. Now reading dataa from stream and writing to output file : " + configuration.getFile());
 		while((read = dis.read(buffer, 0, configuration.getPacketSize())) > 0) {
 			totalRead += read;
-			System.out.println("read " + totalRead + " bytes.");
 			fos.write(buffer, 0, read);
 		}
+		
+		if (totalRead > 0)
+			logger.info("Successfully written " + totalRead + " bytes of data to " + configuration.getFile());
 		
 		fos.close();
 		dis.close();
@@ -62,5 +75,4 @@ public class FileServer extends Thread {
 		FileServer fs = new FileServer(configReader.getConfiguration());
 		fs.start();
 	}
-
 }
